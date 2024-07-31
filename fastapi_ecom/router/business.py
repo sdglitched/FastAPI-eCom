82afabb1d9e8a2acd148fbdd653e1bce740c8bb7
Shounak Dey <shounakdey@ymail.com>
2024-07-31 21:53:09 +0530

    Redefine model and add delete endpoint 1. Add creation date field in orders table using SQLAlchemy mixin 2. Add delete endpoints for customers and businesses

--- fastapi_ecom/router/business.py
+++ fastapi_ecom/router/business.py
@@ -6,7 +6,7 @@ from typing import List
 
 from fastapi_ecom.database.db_setup import get_db
 from fastapi_ecom.database.pydantic_schemas.business import Business, BusinessCreate
-from fastapi_ecom.utils.crud.business import create_business, get_business ,get_business_by_email
+from fastapi_ecom.utils.crud.business import create_business, get_business, get_business_by_email, delete_business
 from fastapi_ecom.utils.auth import verify_business_cred
 
 
@@ -27,3 +27,11 @@ async def read_business_me(email: str = Depends(verify_business_cred)):
 async def read_businesss(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     businesss = get_business(db, skip=skip, limit=limit)
     return businesss
+
+@router.delete("/business/delete/me")
+async def dlt_business(db: Session = Depends(get_db), email: str = Depends(verify_business_cred)):
+    db_business = get_business_by_email(db=db, email=email)
+    if db_business is None:
+        raise HTTPException(status_code=404, detail="Business not present in database")
+    business_to_delete = delete_business(db=db, uuid=db_business.uuid)
+    return business_to_delete
