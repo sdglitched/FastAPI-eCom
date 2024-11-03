48049bc81530f0c9859c963d48db13fd4abc438c
Shounak Dey <shounakdey@ymail.com>
2024-11-03 23:13:38 +0530

    Add schema and endpoints for `order` and `order-details`
    - Add pydantic schema for `order` and `order-details`
    - Add CRUD operations and endpoints for the same
    
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

---
+++ fastapi_ecom/database/pydantic_schemas/order_details.py
@@ -0,0 +1,27 @@
+from abc import ABC
+from typing import List
+
+from pydantic import BaseModel
+
+
+class OrderDetailsBase(BaseModel, ABC):
+    """
+    Base: OrderDetails
+    """
+
+    class Config:
+        from_attributes = True
+
+
+class OrderDetailsCreate(OrderDetailsBase):
+    product_id: str
+    quantity: int
+
+
+class OrderDetailsView(OrderDetailsCreate):
+    price: float
+
+
+class OrderDetailsViewInternal(OrderDetailsView):
+    uuid: str
+    
