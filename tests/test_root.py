e516e2cc8c2e384ded770225720cfb46b7ed1257
Shounak Dey <shounakdey@ymail.com>
2024-12-19 21:25:35 +0530

    Refactor test directory and add root test in it
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

--- tests/test_root.py
+++
@@ -1,8 +0,0 @@
-async def test_root(client):
-    response = await client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {
-        "title": "FastAPI ECOM",
-        "description": "E-Commerce API for businesses and end users using FastAPI.",
-        "version": "0.1.0"
-    }
